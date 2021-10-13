d8a80b18743369921504c290917619bf150f07f8
mudziok <mudziok@gmail.com>
2021-10-13 15:46:44 +0200

    Refactor tsp module

--- tsp.py
+++ tsp.py
@@ -5,47 +5,53 @@ from subprocess import PIPE, Popen
 import numpy as np
 from matplotlib import pyplot as plt
 
-INSTANCE_DIR = "./instances"
+INSTANCE_DIR = "instances"
 
-def load_instance(path):
+def run_exe(filepath, stdin):
+    process = Popen([filepath, 'f'], stdout=PIPE, stdin=PIPE)    
+    stdout = process.communicate(input=str.encode(stdin))[0].decode()
+    return stdout
+
+def load_instance_file(filepath):
     instance = np.array([])
-    instance.shape = (2, 0)
+    instance.shape = (0, 2)
 
-    with open(path, 'r') as f:
+    with open(filepath, 'r') as f:
         n = int(next(f))
         for _ in range(n):
             _, x, y = [int(x) for x in next(f).split()]
-            instance = np.concatenate((instance, np.array([[x], [y]])), axis=1)
+            instance = np.concatenate((instance, np.array([[x, y]])))
     
-    with open(path, 'r') as f:
+    with open(filepath, 'r') as f:
         return (instance, f.read())
 
 def trace(instance, hamilton):
     lines = np.array([])
-    lines.shape = (2, 0)
+    lines.shape = (0, 2)
 
     hamilton.append(hamilton[0])
     for h in hamilton:
-        x, y = instance.T[h - 1].T
-        lines = np.concatenate((lines, np.array([[x], [y]])), axis=1)
-    plt.plot(*lines, color = 'tab:blue')
+        x, y = instance[h - 1]
+        lines = np.concatenate((lines, np.array([[x, y]])))
+    plt.plot(*lines.T, color = 'tab:blue')
 
 def process_instance(filepath):
-    instance, txt = load_instance(filepath)
+    instance, txt = load_instance_file(filepath)
 
-    process = Popen(['greedy.exe', 'f'], stdout=PIPE, stdin=PIPE)    
-    out = process.communicate(input=str.encode(txt))[0].decode()
-    hamilton = [int(x) for x in out.split()]
+    stdout = run_exe('greedy.exe', txt)
+    hamilton = [int(x) for x in stdout.split()]
 
-    plt.scatter(*instance)
+    plt.title(filepath)
+    plt.scatter(*instance.T, color = 'tab:blue')
     trace(instance, hamilton)
 
     plt.show()
 
-if len(sys.argv) == 1:
-    for filename in os.listdir(INSTANCE_DIR):
-        filepath = INSTANCE_DIR + '/' + filename
-        process_instance(filepath)
-else:
-    for filepath in sys.argv[1:]:
-        process_instance(filepath)
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        for filename in os.listdir(INSTANCE_DIR):
+            filepath = INSTANCE_DIR + '/' + filename
+            process_instance(filepath)
+    else:
+        for filepath in sys.argv[1:]:
+            process_instance(filepath)
