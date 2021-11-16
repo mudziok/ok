f524f3020d5fc118140f0f201257097d27ee8301
mudziok <mudziok@gmail.com>
2021-11-16 04:44:21 +0100

    Implement Haskell version of greedy algorithm

---
+++ greedy.hs
@@ -0,0 +1,35 @@
+import Data.List
+import Data.Function
+
+type Point = (Int, Int, Int)
+
+readLine :: String -> Point
+readLine s = 
+    let (a:b:c:_) = map read (words s)
+    in (a,b,c)
+
+parse :: String -> [Point]
+parse xs = map readLine (lines xs)
+
+showLine :: Point -> String
+showLine (a,b,c) = unwords $ map show [a,b,c]
+
+unparse :: [Point] -> String
+unparse xs = unlines $ map showLine xs
+
+distance :: Point -> Point -> Int
+distance (_,x1,y1) (_,x2,y2) = 
+    let dx = x1 - x2
+        dy = y1 - y2
+    in dx^2 + dy^2
+
+solve :: [Point] -> [Point]
+solve [] = []
+solve (x:xs) =
+    let sorted = sortBy (compare `on` (distance x)) xs
+    in [x] ++ solve sorted
+
+main = do
+    n <- getLine
+    testcase <- getContents  
+    putStr $ unlines (map (\(id,_,_) -> (show id) ++ " ") (solve (parse testcase)))
